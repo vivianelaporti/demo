5fa6be06540480568f19d54fa54f0f15b31886d1
Felipe Vilarinho <fvilarinho@gmail.com>
2021-11-23 22:38:03 -0300

    Adding notification in slack.

--- notify.sh
+++ notify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#devops\", \"username\": \"webhookbot\", \"text\": \"This is posted to #devops and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload={\"channel\": \"#devops\", \"username\": \"webhookbot\", \"text\": \"This is posted to #devops and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}" $SLACK_WEBHOOK_URL
