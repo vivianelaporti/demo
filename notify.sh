e7cd610f19181510cd95873630121ae1277a3821
Felipe Vilarinho <fvilarinho@gmail.com>
2021-11-23 22:29:29 -0300

    Adding notification in slack.

---
+++ notify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#devops\", \"username\": \"Demo BOT\", \"text\": \"Pipeline executada com sucesso.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
